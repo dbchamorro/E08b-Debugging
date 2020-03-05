aa6cac20109f00be36db73a2142f0bf073ecc387
Destiny C <dbchamorro@yahoo.com>
2020-03-04 19:45:54 -0500

    6/8 Completed
    I have found 6 out of the 8 bugs in this code.

--- src/Screens/EndScreen.gd
+++ src/Screens/EndScreen.gd
@@ -5,4 +5,5 @@ onready var result: Label = $Result
 
 
 func _ready() -> void:
-	result.text = result.text % [PlayerData.Score, PlayerData.deaths]
+	result.text %= [PlayerData.Score, PlayerData.deaths]
+	print(result.text)
